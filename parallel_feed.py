6b7ec74c322ac33f462fb80ebc8a2789a7e63ffa
grcgrc <grctestemail@gmail.com>
2019-01-18 18:57:07 +0000

    Updated price feed script
    Only publishes feeds once there's a min difference achieved in asset_value

--- parallel_feed.py
+++ parallel_feed.py
@@ -6,7 +6,13 @@ from bitshares.market import Market
 from bitsharesapi.websocket import BitSharesWebsocket
 import pendulum
 import math
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
+
+manager = Manager()
+past_values = manager.dict()
+past_values['URTHR'] = 0.0
+past_values['VERTHANDI'] = 0.0
+past_values['SKULD'] = 0.0
 
 def norn_feed(amplitude, reference_timestamp, current_timestamp, period, phase_offset):
 	"""
@@ -15,12 +21,10 @@ def norn_feed(amplitude, reference_timestamp, current_timestamp, period, phase_o
 	waveform = math.sin(((((current_timestamp - (reference_timestamp + phase_offset))/period) % 1) * period) * ((2*math.pi)/period)) # Only change for an alternative HERTZ ABA.
 	return 1 + (amplitude * waveform)
 
-def multi_feed(target_name, phase_offset):
+def multi_feed(target_name, phase_offset, past_value):
 	"""
 	Publish multiple feeds in parallel
 	"""
-	print("multi " + target_name)
-
 	reference_timestamp = pendulum.parse("2015-10-13T14:12:24+00:00").timestamp() # Retrieving the Bitshares 2.0 genesis block timestamp
 	current_timestamp = pendulum.now().timestamp()
 
@@ -32,31 +36,41 @@ def multi_feed(target_name, phase_offset):
 		phase_offset # phase offset
 		)
 
-	wallet_password = "LOCAL_WALLET_PASSWORD"
-	target_pair = "BTS/"+str(target_name)
-	target = Price(asset_value, target_pair)
-	target.bitshares.wallet.unlock(wallet_password)
+	previously_published_value = past_value[target_name]
+	#print(target_name + " init: " + str(previously_published_value) + " now: " + str(asset_value))
 
-	target.bitshares.publish_price_feed(
-	  target_name,
-	  target,
-	  cer=target*0.8, # Setting in line with Wackou's price feed scripts
-	  mssr=110,
-	  mcr=200,
-	  account="account_name"
-	)
+	if (math.fabs(asset_value - previously_published_value) > 0.00001):
+		past_value[target_name] = asset_value
+		#print(str(math.fabs(asset_value - previously_published_value)))
+		#print((math.fabs(asset_value - previously_published_value) > 0.00001))
+		wallet_password = "LOCAL_WALLET_PASSWORD"
+		target_pair = "BTS/"+str(target_name)
+		target = Price(asset_value, target_pair)
+		target.bitshares.wallet.unlock(wallet_password)
+
+		target.bitshares.publish_price_feed(
+		  target_name,
+		  target,
+		  cer=target*0.8, # Setting in line with Wackou's price feed scripts
+		  mssr=110,
+		  mcr=200,
+		  account="account_name"
+		)
+		print("Published " + target_name)
+	else:
+		print("Skipped block " + target_name)
 
 def publish_wyrd(block_param):
 	"""
 	Triggers every 3 seconds.
 	Calculates then publishes the feeds for urthr, verthandi and skuld.
 	"""
-	print("publishing")
+	print("Attempting to publish")
+
 	with Pool(3) as p:
 		# Parallelizes the feed production
-		p.starmap(multi_feed, [("URTHR",0), ("VERTHANDI",806112), ("SKULD",1612224)])
+		p.starmap(multi_feed, [("URTHR",0,past_values), ("VERTHANDI",806112,past_values), ("SKULD",1612224,past_values)])
 
-	print("published")
 	print("---------")
 
 if __name__ == "__main__":
@@ -66,17 +80,9 @@ if __name__ == "__main__":
 	print("Started")
 	full_node_list = [
 		"wss://eu.nodes.bitshares.works", #location: "Central Europe - BitShares Infrastructure Program"
-		"wss://us.nodes.bitshares.works", #location: "U.S. West Coast - BitShares Infrastructure Program"
-		"wss://sg.nodes.bitshares.works", #location: "Singapore - BitShares Infrastructure Program"
 		"wss://bitshares.crypto.fans/ws", #location: "Munich, Germany"
-		"wss://bit.btsabc.org/ws", #location: "Hong Kong"
 		"wss://api.bts.blckchnd.com" #location: "Falkenstein, Germany"
-		"wss://openledger.hk/ws", #location: "Hong Kong"
-		"wss://bitshares-api.wancloud.io/ws", #location:  "China"
 		"wss://dex.rnglab.org", #location: "Netherlands"
-		"wss://dexnode.net/ws", #location: "Dallas, USA"
-		"wss://kc-us-dex.xeldal.com/ws", #location: "Kansas City, USA"
-		"wss://la.dexnode.net/ws", #location: "Los Angeles, USA"
 	]
 
 	ws = BitSharesWebsocket(
